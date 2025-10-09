02ec09c1a6a459f96c46dd7208bc19796b2a243a
amirIDKxd <amirrezapoorkermani.yt@gmail.com>
2025-10-09 16:59:17 +0330

    Basic Movement Module (without camera Rotation)
    Created a scene with player collision and coded the movement for the player without the mouse movement.

--- Program/Player.cs
+++ Program/Player.cs
@@ -1,15 +1,18 @@
 using Godot;
 using System;
 
-public partial class Player : Node
+public partial class Player : CharacterBody3D
 {
+	private PlayerMovement Movement;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
-	{
-	}
+    {
+        Movement= new PlayerMovement(this);
+    }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
-	{
-	}
+    {
+		Movement.UpdateMovement(delta);
+    }
 }
--- Program/PlayerProperty.cs
+++ Program/PlayerProperty.cs
@@ -1,6 +1,6 @@
 using Godot;
 using System;
 
-public partial class PlayerProperties : GodotObject
+public partial class PlayerProperty : GodotObject
 {
 }
