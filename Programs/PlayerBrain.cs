0c889a7ed8ad3363db167e944fbef7d0869eda04
amirIDKxd <amirrezapoorkermani.yt@gmail.com>
2025-10-10 22:45:26 +0330

    Pain...suffering...
    the c# tutorials for godot are just causing error after error for me somehow and every good tutorial is on GDscript, time to switch for other Logics (PlayerInteract is not working but no i am not deleting anything)

--- Programs/PlayerBrain.cs
+++ Programs/PlayerBrain.cs
@@ -1,24 +1,27 @@
 using Godot;
 using System;
-
 public partial class PlayerBrain : CharacterBody3D
 {
-	[Export]public float Speed = 5.0f;
+	[Export] public float Speed = 5.0f;
 	[Export] public float JumpVelocity = 4.5f;
+	[Export] public NodePath RayCastPath { get; set; }
 	private PlayerMovement Movement;
-	// Called when the node enters the scene tree for the first time.
+	//private PlayerInteract Interact;
 	public override void _Ready()
 	{
-		Movement = new PlayerMovement(this,Speed,JumpVelocity);
-    }
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
+		Movement = new PlayerMovement(this, Speed, JumpVelocity);
+		//Interact = new PlayerInteract(RayCastPath);
+		//Interact._Ready();
+		//Interact.check();
+	}
 	public override void _Process(double delta)
 	{
 		Movement.UpdateMovement(delta);
+		//Interact.Update();
 	}
 	public override void _Input(InputEvent @event)
-    {
-        Movement.HandleInput(@event);
-    }
+	{
+		Movement.HandleInput(@event);
+	}
 }
+//Godot C# is pain, maybe i should have started with gdscript 😭😭😭😭😭
--- Programs/PlayerMouseLook.cs
+++ Programs/PlayerMouseLook.cs
@@ -1,40 +1,40 @@
 using Godot;
 using System;
-public partial class PlayerMouseLook : Camera3D
+public partial class PlayerMouseLook : Node3D
 {
-    [Export] public NodePath PlayerNodePath { get; set; }
-    private Node3D _playerNode;
-    [Export] public float Sensitivity { get; set; } = 0.005f;
-    [Export(PropertyHint.Range, "-1.57,0.0,0.01")]
-    public Vector2 VertRotLimit { get; set; } = new Vector2(-Mathf.Pi / 2, Mathf.Pi / 2);
-    [Export] public float MaxFps { get; set; } = 10f;
-    public override void _Ready()
-    {
-        Input.MouseMode = Input.MouseModeEnum.Captured;
-        if (PlayerNodePath != null && !PlayerNodePath.IsEmpty)
-        {
-            _playerNode = GetNode<Node3D>(PlayerNodePath);
-        }
-    }
-    public override void _Input(InputEvent @event)
-    {
-        // Safety check
-        if (_playerNode == null) return;
-        if (@event is InputEventMouseMotion MouseMovement)
-        {
-            Vector2 velocity = MouseMovement.Relative * Sensitivity;
-            velocity = new Vector2(
-                Godot.Mathf.Clamp(velocity.X, -MaxFps, MaxFps),
-                Godot.Mathf.Clamp(velocity.Y, -MaxFps, MaxFps)
-            );
-            Vector3 camRotation = Rotation;
-            camRotation.X += -velocity.Y;
-            camRotation.X = Godot.Mathf.Clamp(camRotation.X, VertRotLimit.X, VertRotLimit.Y);
+	[Export] public NodePath PlayerNodePath { get; set; }
+	private Node3D _playerNode;
+	[Export] public float Sensitivity { get; set; } = 0.005f;
+	[Export(PropertyHint.Range, "-1.57,0.0,0.01")]
+	public Vector2 VertRotLimit { get; set; } = new Vector2(-Mathf.Pi / 2, Mathf.Pi / 2);
+	[Export] public float MaxFps { get; set; } = 10f;
+	public override void _Ready()
+	{
+		Input.MouseMode = Input.MouseModeEnum.Captured;
+		if (PlayerNodePath != null && !PlayerNodePath.IsEmpty)
+		{
+			_playerNode = GetNode<Node3D>(PlayerNodePath);
+		}
+	}
+	public override void _Input(InputEvent @event)
+	{
+		// Safety check
+		if (_playerNode == null) return;
+		if (@event is InputEventMouseMotion MouseMovement)
+		{
+			Vector2 velocity = MouseMovement.Relative * Sensitivity;
+			velocity = new Vector2(
+				Godot.Mathf.Clamp(velocity.X, -MaxFps, MaxFps),
+				Godot.Mathf.Clamp(velocity.Y, -MaxFps, MaxFps)
+			);
+			Vector3 camRotation = Rotation;
+			camRotation.X += -velocity.Y;
+			camRotation.X = Godot.Mathf.Clamp(camRotation.X, VertRotLimit.X, VertRotLimit.Y);
 
-            Rotation = camRotation;
-            _playerNode.RotateY(-velocity.X);
-        }
-    }
+			Rotation = camRotation;
+			_playerNode.RotateY(-velocity.X);
+		}
+	}
 }
 // This piece of code was possible by watching Royas Godot's "Godot 4 C# FPS Controller" youtube tutorial and fixing errors that came up by asking Google Gemini about the said errors.
 //i do not know why i would get errors when i followed all of the steps correctly, my guess would be Godot Version Differences (i am using a newer version (4.5)) but i've been wrong multiple times.
